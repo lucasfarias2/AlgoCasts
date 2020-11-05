6cb8a9e2abbba526927f0f77bfe9eaf2f936e0f2
Lucas Farias <lukf95@gmail.com>
2020-11-04 23:17:33 -0300

    Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,28 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//      '   #   '
+//      '  ###  '
+//      ' ##### '
+//      '#######'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midPoint = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (midPoint - row <= col && midPoint + row >= col) {
+        level = level + '#';
+      } else {
+        level = level + ' ';
+      }
+    }
+    
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
