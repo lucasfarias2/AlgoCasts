34d898a68e455db9c35950149b4106ea43e1bea6
Lucas Farias <lukf95@gmail.com>
2020-11-14 22:38:54 -0300

    Matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -6,15 +6,57 @@
 //     [[1, 2],
 //     [4, 3]]
 //   matrix(3)
-//     [[1, 2, 3],
-//     [8, 9, 4],
-//     [7, 6, 5]]
+//         0  1  2
+//     0 [[1, 2, 3],
+//     1 [8, 9, 4],
+//     2 [7, 6, 5]]
 //  matrix(4)
 //     [[1,   2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  
+  while(startColumn <= endColumn && startRow <= endRow) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    for(let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+
+
+  return results;
+}
 
 module.exports = matrix;
